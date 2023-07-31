1b6e3facb38be156ed5db8cb6383619dbf9a2bc2
Mayank <mynk1997@gmail.com>
2023-07-31 18:09:49 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div>Hello</div>
+  );
+}
+
+export default App;
